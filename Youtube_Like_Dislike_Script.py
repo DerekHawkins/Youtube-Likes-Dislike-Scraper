f41b36e3eff542be80f490a1d25c04cafd8797ad
DerekHawkins <38869302+DerekHawkins@users.noreply.github.com>
2018-07-03 11:05:10 -0400

    Update Youtube_Like_Dislike_Script.py

--- Youtube_Like_Dislike_Script.py
+++ Youtube_Like_Dislike_Script.py
@@ -8,12 +8,14 @@ Created on Tue Jul  3 10:31:56 2018
 import pafy
 import pandas as pd
 
+
 path = 'C:/Users/Derek.Hawkins/Documents/Python Training/Youtube_URLs.xlsx'
 data = pd.read_excel(path)
 
-videos = data['Youtube Links']    
+videos = data['Youtube Links'] #change string to whatever your column title is 
 dataSheet = []
 
+#iterates through excel file, note that this loop is calling on a singular basis so expect slower run time
 for i in videos:
     clips = pafy.new(i)
     titles = clips.title
@@ -26,4 +28,5 @@ df = pd.DataFrame(dataSheet, columns = ['Video Titles', 'Likes', 'Dislikes'])
 df["URLs"] = videos
 print(df)
 
+#save to csv 
 #df.to_csv('C:/Users/Derek.Hawkins/Documents/Python Training/Youtube_MetaData.csv')
